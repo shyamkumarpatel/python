2cb4dc87fdee51829dc528aea7c57b1e78e94680
Shyam Patel <shyam.psu.lv@gmail.com>
2022-06-06 14:42:09 -0400

    adding comments

--- fundamentals/fundamentals/file.py
+++ fundamentals/fundamentals/file.py
@@ -1,17 +1,17 @@
-num1 = 42
-num2 = 2.3
-boolean = True
-string = 'Hello World'
-pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives']
-person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False}
-fruit = ('blueberry', 'strawberry', 'banana')
-print(type(fruit))
-print(pizza_toppings[1])
+num1 = 42 #Variable declaration
+num2 = 2.3 #Variable declaration
+boolean = True #Variable declaration
+string = 'Hello World' #Variable declaration
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives'] #Variable declaration
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False} #Variable declaration
+fruit = ('blueberry', 'strawberry', 'banana') #Variable declaration
+print(type(fruit))  #log statement
+print(pizza_toppings[1]) #log statement
 pizza_toppings.append('Mushrooms')
-print(person['name'])
+print(person['name']) #log statement
 person['name'] = 'George'
 person['eye_color'] = 'blue'
-print(fruit[2])
+print(fruit[2]) #log statement
 
 if num1 > 45:
     print("It's greater")
@@ -19,11 +19,11 @@ else:
     print("It's lower")
 
 if len(string) < 5:
-    print("It's a short word!")
+    print("It's a short word!") #log statement
 elif len(string) > 15:
-    print("It's a long word!")
+    print("It's a long word!") #log statement
 else:
-    print("Just right!")
+    print("Just right!") #log statement
 
 for x in range(5):
     print(x)
@@ -39,32 +39,32 @@ while(x < 5):
 pizza_toppings.pop()
 pizza_toppings.pop(1)
 
-print(person)
+print(person) #log statement
 person.pop('eye_color')
-print(person)
+print(person) #log statement
 
 for topping in pizza_toppings:
     if topping == 'Pepperoni':
         continue
-    print('After 1st if statement')
+    print('After 1st if statement') #log statement
     if topping == 'Olives':
         break
 
 def print_hello_ten_times():
     for num in range(10):
-        print('Hello')
+        print('Hello') #log statement
 
 print_hello_ten_times()
 
 def print_hello_x_times(x):
     for num in range(x):
-        print('Hello')
+        print('Hello') #log statement
 
 print_hello_x_times(4)
 
 def print_hello_x_or_ten_times(x = 10):
     for num in range(x):
-        print('Hello')
+        print('Hello') #log statement
 
 print_hello_x_or_ten_times()
 print_hello_x_or_ten_times(4)
