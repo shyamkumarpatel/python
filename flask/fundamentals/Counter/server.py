606df5118b99bc095b31f8ab1f36200ff0221aea
Shyam Patel <shyam.psu.lv@gmail.com>
2022-06-14 14:40:16 -0400

    adding button functionality

--- flask/fundamentals/Counter/server.py
+++ flask/fundamentals/Counter/server.py
@@ -5,8 +5,16 @@ app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')
 def index():
-    return render_template("index.html",numCount = 1)
+    if "num_of_visits" in session:
+        session["num_of_visits"] += 1
+    else:
+        session["num_of_visits"] = 1
+    return render_template("index.html")
 
-    
+@app.route('/destroy_session')
+def reset():
+    session.clear()
+    return redirect("/")
+
 if __name__=="__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
